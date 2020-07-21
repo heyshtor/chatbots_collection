39a22f9cb5cf056f78736d3d010c7ec89c021630
heyshtor <mheyshtor@gmail.com>
2020-07-21 17:31:41 +0100

    Coffee bot v.2

--- Coffee_chatbot.py
+++ Coffee_chatbot.py
@@ -1,72 +1,54 @@
 # This is a Codecademy assignment for a small system-initiative chatbot.
+from utils import print_message, get_size, order_latte, order_mocha, choose_cup, yes_list, no_list
 
-# Main bot
 def coffee_bot():
-  print("Welcome to the cafe!")
+  print('Welcome to the cafe!')
 
-  size = get_size()
-  drink_type = get_drink_type()
-  cup = choose_cup()
-  print("Alright, that's a {} {} in a {}!".format(size, drink_type, cup))
+# Order the first drink
+  order_drink = 'y'
+# List to store drinks
+  drinks = []
 
-  name = input("Can I get your name please? \n")
-  print("Thanks, {}! Your drink will be ready shortly.".format(name))
+# Ordering the drink - collecting the order
+  while order_drink in yes_list:
+     size = get_size()  
+     drink_type = get_drink_type()
+     cup = choose_cup()
 
 
-# Ask for the size of the drink
-def get_size():
-  res = input("What size drink can I get for you? \n[a] Small \n[b] Medium \n[c] Large \n ")
+     drink = '{} {} in a {}'.format(size, drink_type, cup)
+     print('Alright, that\'s a {}!'.format(drink))
+     drinks.append(drink)
 
-  if res == 'a':
-    return 'small'
-  elif res == 'b':
-    return 'medium'
-  elif res == 'c':
-    return 'large'
-  else:
-    print_message()
-    return get_size() # now it becomes recursive function
+    # Keep asking if the customer wanted another drink if they enter anything besides yes or no
+     while True:
+      order_drink = input('Would you like to order another drink? (y/n) \n ')
+      if (order_drink in yes_list) or (order_drink in no_list):
+        break
+  
+# Print all drinks the customer has ordered, iterating through the list of drinks:
+  print('So, I have: ')
+  for drink in drinks:
+    print('- ' + drink)
 
-# Generic error message for when no option is provided
-def print_message():
-  print("I'm sorry, I did not understand your selection. Please enter the corresponding letter for your response.")
+  # Ask for the customer's name
+  name = input('Can I get your name please? \n> ')
+
+  print('Thanks, {}! Your order will be ready shortly.'.format(name))
 
 # Ask for the drink order
 def get_drink_type():
-  res = input("What type of drink would you like? \n[a] Brewed Coffee \n[b] Mocha \n[c] Latte \n ")
+  res = input('What type of drink would you like? \n[a] Brewed Coffee \n[b] Mocha \n[c] Latte \n> ')
+
   if res == 'a':
     return 'brewed coffee'
   elif res == 'b':
-    return 'mocha'
+    return order_mocha()
   elif res == 'c':
     return order_latte()
   else:
     print_message()
-    return get_drink_type() 
-
-# Function for latte
-def order_latte():
-  res = input("And what kind of milk for your latte? \n[a] 2% milk \n[b] Non-fat milk \n[c] Soy milk \n ")
-  if res == 'a':
-    return 'latte'
-  elif res == 'b':
-    return 'non-fat latte'
-  elif res == 'c':
-    return 'soy latte'
-  else:
-    print_message()
-    return order_latte() 
-
-# Ask about a cup
-def choose_cup():
-  res = input("Would you like a takeaway plastic cup or do you have a reusable cup? \n[a] Plastic cup \n[b] Reusable cup \n ")
-  if res == 'a':
-    return 'plastic cup'
-  elif res == 'b':
-    return 'reusable cup'
-  else:
-    print_message()
-    return choose_cup()
-
-# Calling the bot
+    return get_drink_type()
+  
+# Call the bot
 coffee_bot()
---
+++ utils.py
@@ -0,0 +1,56 @@
+
+# Ask for the size of the drink
+def get_size():
+  res = input("What size drink can I get for you? \n[a] Small \n[b] Medium \n[c] Large \n ")
+
+  if res == 'a':
+    return 'small'
+  elif res == 'b':
+    return 'medium'
+  elif res == 'c':
+    return 'large'
+  else:
+    print_message()
+    return get_size() # now it becomes recursive function
+
+# Function for latte
+def order_latte():
+  res = input("And what kind of milk for your latte? \n[a] 2% milk \n[b] Non-fat milk \n[c] Soy milk \n ")
+  if res == 'a':
+    return 'latte'
+  elif res == 'b':
+    return 'non-fat latte'
+  elif res == 'c':
+    return 'soy latte'
+  else:
+    print_message()
+    return order_latte() 
+
+# Function for mocha
+def order_mocha():
+  while True:
+    res = input('Would you like to try our limited-edition peppermint mocha? \n[a] Sure! \n[b] Maybe next time! \n ')
+    if res == 'a':
+      return 'pepperming mocha'
+    elif res == 'b':
+      return 'mocha'
+    
+    print_message()
+
+# Ask about a cup
+def choose_cup():
+  res = input("Would you like a takeaway plastic cup or do you have a reusable cup? \n[a] Plastic cup \n[b] Reusable cup \n ")
+  if res == 'a':
+    return 'plastic cup'
+  elif res == 'b':
+    return 'reusable cup'
+  else:
+    print_message()
+    return choose_cup()
+
+# Generic error message for when no option is provided
+def print_message():
+  print("I'm sorry, I did not understand your selection. Please enter the corresponding letter for your response.")
+
+yes_list = ['y', 'yes', 'yeah', 'ok', 'sure']
+no_list = ['n', 'no', 'not', 'stop', 'bye']
